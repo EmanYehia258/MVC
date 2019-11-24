c3d7b666df85187ff36b0a9683e384a62e426a9a
EmanYehia258 <56773982+EmanYehia258@users.noreply.github.com>
2019-11-24 22:55:21 +0200

    controllers,model,views

---
+++ LasttaskMVC/Controllers/CategoriesController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LasttaskMVC.Models;
+
+namespace LasttaskMVC.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private NorthwindEntities2 db = new NorthwindEntities2();
+
+        // GET: Categories
+        public ActionResult Index()
+        {
+            return View(db.Categories.ToList());
+        }
+
+        // GET: Categories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // GET: Categories/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Categories/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CategoryID,CategoryName")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categories.Add(category);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
+        }
+
+        // GET: Categories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Categories/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CategoryID,CategoryName")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(category).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        // GET: Categories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Categories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var res=db.Products.Where(a => a.CategoryID == id).FirstOrDefault();
+            if (res != null)
+            {
+                db.Products.Remove(res);
+                Category category = db.Categories.Find(id);
+                db.Categories.Remove(category);
+                db.SaveChanges();
+            }else
+            {
+                Category category = db.Categories.Find(id);
+                db.Categories.Remove(category);
+                db.SaveChanges();
+
+            }
+            
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ LasttaskMVC/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LasttaskMVC.Models;
+namespace LasttaskMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        NorthwindEntities2 db = new NorthwindEntities2();
+        // GET: Home
+        public ActionResult Index()
+        {
+          var list=  db.Products.ToList();
+            return View(list);
+        }
+        public ActionResult insrt()
+        {
+            return View();
+        }
+        
+        public ActionResult insert(User u)
+        {
+         
+            db.Users.Add(u);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string email, string pasword)
+        {
+            if (email == "admin@gmail.com" && pasword == "admin")
+            {
+                return RedirectToAction("admin");
+            }
+            else
+            {
+                try
+                {
+                    int id = db.Users.Where(a => a.email == email).ToList().FirstOrDefault().ID;
+                    int idd = db.Users.Where(u => u.password == pasword).ToList().FirstOrDefault().ID;
+                    if (id >= 0 && idd >= 0 && id == idd)
+                    {
+                        string name = db.Users.Where(a => a.ID == id).ToList().FirstOrDefault().name;
+                        TempData["name"] = name;
+                        return RedirectToAction("welcome");
+
+                    }
+                    else
+                    {
+                        return RedirectToAction("error");
+                    }
+                }
+                catch (Exception e)
+                {
+                    return RedirectToAction("error");
+                }
+            }
+            
+        }
+        public ActionResult welcome()
+        {
+            return View();
+        }
+        public ActionResult error()
+        {
+            return View();
+        }
+   
+        public ActionResult admin()
+        {
+            return View();
+        }
+
+    }
+}
